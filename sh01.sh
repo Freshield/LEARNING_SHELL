40d6a8cdee2c54fad39dcd8040538b940bece302
Freshield <zxdsw199182@gmail.com>
2016-01-17 21:56:29 -0500

    6

---
+++ sh01.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Program:
+#	Tis program shows 'Hello World!" in your sreen.
+#History:
+#2016/01/16 Freshield First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+echo -e "Hello World! \a \n"
+exit 0
